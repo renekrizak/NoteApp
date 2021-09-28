36892af09042ec58e112d0126859e4f0dfb842c2
renekrizak <rene.krizak@gmail.com>
2021-09-28 18:03:39 +0200

    Zmena pozadia + hotovy AddNewNoteButton s animaciami, potreba pridat plus znak a nasledne funkcionalitu tlacitka

--- NoteApp/MainWindow.xaml.cs
+++ NoteApp/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace NoteApp
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
     }
 }
